b50960dcf451ce7f4cd3d85d96825009e22ae8c0
oxcl <oxcl@proton.me>
2026-06-16 08:26:31 +0400

    remove all e2e tests

--- test/e2e/proxy/auth-valid.route.ts
+++
@@ -1,8 +0,0 @@
-import type { Proxy } from '../../../src';
-
-// biome-ignore lint/correctness/noUnusedVariables: consistent handler signature
-export async function handler(proxy: Proxy, _env?: unknown): Promise<Response> {
-  const conn = await proxy.connect({ host: 'httpbin.org', port: 443, tls: true });
-  conn.close();
-  return new Response('OK');
-}
--- test/e2e/test-fetch/redirect.route.ts
+++
@@ -1,8 +0,0 @@
-import type { Proxy } from '../../../src';
-import { socksFetch } from '../fetch-wrapper';
-import { HTTPBIN_BASE } from '../helpers';
-
-// biome-ignore lint/correctness/noUnusedVariables: consistent handler signature
-export async function handler(proxy: Proxy, _env?: unknown): Promise<Response> {
-  return socksFetch(`${HTTPBIN_BASE}/redirect/2`, { proxy });
-}
